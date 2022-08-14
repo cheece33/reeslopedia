4cb8bb1d92e0bea23f29c454382ef24cc517bad9
Reece Santos <109492774+cheece33@users.noreply.github.com>
2022-08-14 12:11:38 +0800

    Delete script.js

--- portfolio v0.4/script.js
+++
@@ -1,7 +0,0 @@
-var easterEgg = document.getElementById("text-easter-egg");
-
-easterEgg.addEventListener("mouseenter", mouseEnterContainer);
-
-function mouseEnterContainer() {
-    console.log('go to background page');
-}
