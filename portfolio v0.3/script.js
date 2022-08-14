4b27899ee38cf15920bea37977c19f0d5f6283c0
Reece Santos <109492774+cheece33@users.noreply.github.com>
2022-08-14 12:17:09 +0800

    Add files via upload

---
+++ portfolio v0.3/script.js
@@ -0,0 +1,7 @@
+var easterEgg = document.getElementById("text-easter-egg");
+
+easterEgg.addEventListener("mouseenter", mouseEnterContainer);
+
+function mouseEnterContainer() {
+    console.log('go to background page');
+}
